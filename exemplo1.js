5767f98e119f066d5a359d55d52577dad1814399
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-06-13 18:59:46 -0300

    ADD EX.JSON

--- exemplo1.js
+++ exemplo1.js
@@ -2,4 +2,6 @@ let textoJSON = '{"produto": "Teclado","preco": 119.90,"estoque": 80,"categorias
 
 let acessorio = JSON.parse(textoJSON);
 
+console.log(acessorio)
+console.log(acessorio.produto)
 console.log(textoJSON)
